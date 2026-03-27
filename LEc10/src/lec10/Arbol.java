9074ba9ecd677274d964f7bf04cdcc25b7ca2e6e
ti2myy-jpg <ti2myy@gmail.com>
2026-03-26 19:30:45 -0600

    Continuacion de semana 10(Arboles)

--- LEc10/src/lec10/Arbol.java
+++ LEc10/src/lec10/Arbol.java
@@ -11,7 +11,9 @@ package lec10;
 public class Arbol {
 
     private Nodo raiz;
-
+public Arbol(){
+    raiz = null;
+}
     public void insertar(int valor) {
         raiz = insertarRec(raiz, valor);
     }
@@ -37,8 +39,77 @@ public class Arbol {
     private void inordenRec(Nodo raiz) {
         if (raiz != null) {
             inordenRec(raiz.getIzquierda());
-            System.out.println(raiz.getDato()+ " ");
+            System.out.println(raiz.getDato() + " ");
             inordenRec(raiz.getDerecha());
         }
     }
+
+    public void preorden() {
+        preordenRec(raiz);
+        System.out.println();
+    }
+
+    private void preordenRec(Nodo raiz) {
+        if (raiz != null) {
+            System.out.println(raiz.getDato() + " ");
+            inordenRec(raiz.getIzquierda());
+            inordenRec(raiz.getDerecha());
+        }
+    }
+
+    public void postorden() {
+        postordenRec(raiz);
+        System.out.println();
+    }
+
+    private void postordenRec(Nodo raiz) {
+        if (raiz != null) {
+            inordenRec(raiz.getIzquierda());
+            inordenRec(raiz.getDerecha());
+            System.out.println(raiz.getDato() + " ");
+        }
+    }
+
+    public void eliminar(int valor) {
+        raiz = eliminarRec(raiz, valor);
+    }
+
+    private Nodo eliminarRec(Nodo raiz, int valor) {
+        if (raiz == null) {
+            return raiz;
+        }
+        if (valor < raiz.getDato()) {
+            raiz.setIzquierda(eliminarRec(raiz.getIzquierda(), valor));
+
+        } else if (valor > raiz.getDato()) {
+            raiz.setDerecha(eliminarRec(raiz.getDerecha(), valor));
+        } else {
+            //Caso 1 Nodo sin hijos
+            if (raiz.getIzquierda() == null && raiz.getDerecha() == null) {
+                return null;
+            }
+            //Caso 2 Nodo con un hijo
+            if (raiz.getIzquierda() == null) {
+                return raiz.getDerecha();
+
+            } else if (raiz.getDerecha() == null) {
+                return raiz.getIzquierda();
+
+            }
+            //Caso 3 Nodo con dos hijos
+            Nodo sucesor = minValorNodo(raiz.getDerecha());
+            raiz.setDato(sucesor.getDato());
+            raiz.setDerecha(eliminarRec(raiz.getDerecha(), sucesor.getDato()));
+
+        }
+        return raiz;
+    }
+
+    private Nodo minValorNodo(Nodo nodo) {
+        while (nodo.getIzquierda() != null) {
+            nodo = nodo.getIzquierda();
+        }
+        return nodo;
+    }
+
 }
--- LEc10/src/lec10/Lec10.java
+++ LEc10/src/lec10/Lec10.java
@@ -14,7 +14,30 @@ public class Lec10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Arbol arbol = new Arbol();
+
+        //Insertar
+        arbol.insertar(50);
+        arbol.insertar(30);
+        arbol.insertar(70);
+        arbol.insertar(20);
+        arbol.insertar(40);
+        arbol.insertar(60);
+        arbol.insertar(80);
+        //inorden
+
+        System.out.println("Recorrido inorden");
+        arbol.inorden();
+
+        //Eliminar
+        arbol.eliminar(40);
+        arbol.eliminar(30);
+        arbol.eliminar(70);
+        
+        //posorden
+        System.out.println("Recorrido postorden");
+        arbol.postorden();
+
     }
-    
+
 }
